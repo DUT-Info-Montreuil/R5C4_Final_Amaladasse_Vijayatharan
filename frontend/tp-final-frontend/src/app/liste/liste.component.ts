fdd6947c0dce71e1a70499a25353a8c8c3306b93
VVithushan <vvijayatharan@iut.univ-paris8.fr>
2024-12-13 16:02:19 +0100

    2eme page

--- frontend/tp-final-frontend/src/app/liste/liste.component.ts
+++ frontend/tp-final-frontend/src/app/liste/liste.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-liste',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './liste.component.html',
-  styleUrl: './liste.component.css'
+  styleUrls: ['./liste.component.css']
 })
-export class ListeComponent {
+export class ListeComponent implements OnInit {
+  data: any[] = []; 
+  paginatedData: any[] = [];
+  currentPage: number = 1;
+  itemsPerPage: number = 30;
 
+  constructor(private readonly http: HttpClient) {}
+
+  ngOnInit(): void {
+
+    this.http.get<any[]>('http://127.0.0.1:5000/searches/all').subscribe((response) => {
+      this.data = response;
+      this.updatePagination(); 
+    });
+  }
+
+  updatePagination(): void {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = this.currentPage * this.itemsPerPage;
+    this.paginatedData = this.data.slice(startIndex, endIndex);
+  }
+
+  nextPage(): void {
+    if (this.currentPage * this.itemsPerPage < this.data.length) {
+      this.currentPage++;
+      this.updatePagination();
+    }
+  }
+
+  prevPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updatePagination();
+    }
+  }
 }
